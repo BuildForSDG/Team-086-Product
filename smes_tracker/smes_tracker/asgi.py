1eccd1a148ed987f0d6e02911d569d426ad9a484
bobeu <bobmatea27@gmail.com>
2020-05-15 16:15:06 +0100

    reformatted black, change in .confidential, main.yml, other

--- smes_tracker/smes_tracker/asgi.py
+++ smes_tracker/smes_tracker/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for smeTracker project.
+ASGI config for smes_tracker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smeTracker.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smes_tracker.settings")
 
 application = get_asgi_application()
--- tests/test_src.py
+++ tests/test_src.py
@@ -2,4 +2,8 @@ from src import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_bar():
+    assert True
